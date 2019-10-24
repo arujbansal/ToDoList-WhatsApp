63abaa50bf8948af3df5cc70e0e1801aa2081767
Aruj Bansal <arujbansal1@gmail.com>
2019-10-24 19:31:09 +0530

    Added formatting to dictionary commands list

--- helper_functions.py
+++ helper_functions.py
@@ -7,3 +7,23 @@ def info_in_db(cur, table_name):
     """
     info = cur.execute(f"SELECT * FROM {table_name}")
     return [i for i in info]
+
+
+def format_command_dict(commands_dict):
+    """
+    Formats the commands dictionary in a more readable manner.
+    Adds a serial number to each command.
+    Eg) 1. Help - Available commands.
+
+    At the end of each command, there is '.\n'. This is used for displaying each command on the next line.
+    :param commands_dict: The dictionary containing the commands.
+    :return: A string ready to be sent to the user.
+    """
+    final_msg = []
+    for k, v in commands_dict:
+        final_msg.append(f'{k} - {v}')
+
+    for i in range(len(final_msg)):
+        final_msg[i] = f'{str(i)}. {final_msg[i]}'
+
+    return ".\n".join(final_msg)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import sqlite3
-from helper_functions import info_in_db
+from helper_functions import info_in_db, format_command_dict
 
 app = Flask(__name__)
 
 con = sqlite3.connect('user_info.db', check_same_thread=False)  # Database connection.
 c = con.cursor()  # Database connection cursor.
+
+available_commands = {'H': 'Available commands'}  # Command : Description
 
 
 @app.route('/', methods=['POST'])
@@ -15,7 +17,11 @@ def whatsapp_message():
     message_body = request.form['Body']  # The body of the message.
     resp = MessagingResponse()
 
-    db_users_table = info_in_db(c, "Users")  # Gets all the information present in the Users table.
+    db_users_table = info_in_db(c, "Users")  # Gets all the information present in the Users table
+    msg = ""
+
+    if message_body.lower() == 'h':
+        msg = format_command_dict(available_commands)
 
     return 'Hello World'
 
