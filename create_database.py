39447d58444e30357a7cab3c0d7c14e66b222dc6
Aruj Bansal <arujbansal1@gmail.com>
2019-10-24 17:17:34 +0530

    Final commit

--- create_database.py
+++ create_database.py
@@ -1,7 +1,13 @@
 import sqlite3
 
+#######################################################
+# Run this file if the database does not already exist.
+#######################################################
+
 conn = sqlite3.connect('user_info.db')  # Creates a new database
 c = conn.cursor()
 
-# Creating Users table
+# Creating the Users table
 c.execute('''CREATE TABLE Users (ID INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT NOT NULL, Phone TEXT NOT NULL)''')
+
+conn.commit()  # Save the Users table
